9c7cf8a3bcc414e5240e8b3d3d5b9d95dfafe018
Anil P <anil.prasad@tutanota.com>
2022-11-02 11:50:09 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "Multiformat",
     // Necessary for swift crypto
     platforms: [
-        .macOS(.v10_15),
+        .iOS(.v14),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
